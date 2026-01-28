927baa8c01d1b4476f47ecd013195cb0b10c6cdc
Taffytheking06 <davidtmusk06@gmail.com>
2026-01-29 00:11:45 +0530

    Frontend Finishing Touches

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -3,12 +3,12 @@ import api from "../api"
 import { useNavigate } from "react-router-dom"
 import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants"
 import "../styles/Form.css"
+import LoadingIndicator from "./LoadingIndicator"
 
 function Form({route, method}) {
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
     const [loading, setLoading] = useState(false)
-    const [error, setError] = useState("")
     const navigate = useNavigate()
 
     const name = method === "login" ? "Login" : "Register"
@@ -16,7 +16,6 @@ function Form({route, method}) {
     const handleSubmit = async (e) => {
         setLoading(true)
         e.preventDefault()
-        setError("")
 
         try { 
             const res = await api.post(route, { username, password})
@@ -28,11 +27,7 @@ function Form({route, method}) {
                 navigate("/login")
             }
         } catch (error) {
-            if (error.response?.data) {
-                setError(JSON.stringify(error.response.data))
-            } else {
-                setError(error.message)
-            }
+            alert(error)
         } finally {
             setLoading(false)
         }
@@ -40,7 +35,6 @@ function Form({route, method}) {
 
     return <form onSubmit={handleSubmit} className="form-container">
         <h1>{name}</h1>
-        {error && <div className="error-message">{error}</div>}
         <input
             className="form-input"
             type="text"
@@ -55,6 +49,7 @@ function Form({route, method}) {
             onChange={(e) => setPassword(e.target.value)}
             placeholder="Password"
         />
+        {loading && <LoadingIndicator />}
         <button className="form-button" type="submit">
             {name}
         </button>
